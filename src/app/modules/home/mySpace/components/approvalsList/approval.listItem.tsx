ae719649123c73a3c3d98329f0e6b88e8f0f839b
Umang Joshi <dev.umangjoshi@gmail.com>
2026-03-02 20:58:35 +0530

    Bug resolved

--- src/app/modules/home/mySpace/components/approvalsList/approval.listItem.tsx
+++ src/app/modules/home/mySpace/components/approvalsList/approval.listItem.tsx
@@ -1,8 +1,7 @@
 import { LeaveType } from "@modules/leaveTracker/types/leaves.types";
-import { Button, Card, Checkbox } from "antd";
+import { Avatar, Button, Card, Checkbox } from "antd";
 import { FC, useState } from "react";
 import ConfirmApprovalModal from "../modals/confirmApproval.modal";
-import { Templates } from "@components/templates/templates";
 import { useURLParams } from "@common/hooks/useURLParams";
 
 type Props = {
@@ -18,37 +17,48 @@ const ApprovalListItem: FC<Props> = ({ item, ...p }) => {
   const { setParam } = useURLParams();
   return (
     <Card
-      onClick={() => setParam("employeeId", item.employeeId)}
+      // onClick={() => setParam("employeeId", item.employeeId)}
       hoverable
-      className="mb-default! group"
+      className="mb-default! group/card"
       size="small"
     >
-      <div className="flex items-center justify-between">
-        <div className="flex inline-[50%] gap-default items-center">
+      <div className="grid grid-cols-[1fr_120px_25%] gap-default">
+        <div
+          className="flex w-full gap-default items-center group/info"
+          onClick={() => setParam("employeeId", item.employeeId)}
+        >
           <div className="px-default">
             {p.selected !== undefined && (
               <Checkbox
                 checked={p.selected}
-                onChange={(e) => p.onSelectChange?.(e.target.checked, item)}
+                onClick={(e) => e.stopPropagation()}
+                onChange={(e) => {
+                  e.stopPropagation();
+                  p.onSelectChange?.(e.target.checked, item);
+                }}
               />
             )}
           </div>
 
-          <Templates.EmployeeInfo
-            info={item}
-            extra={
+          <div className="flex gap-default items-center">
+            <Avatar shape="square" src={item?.photoURL} />
+            <div>
+              <div className="group-hover/info:text-active">
+                <span className="text-muted ">{item?.employeeId}</span>
+                <span> - {item?.name || "N/A"}</span>
+              </div>
               <div>
                 <span className="text-muted">has made a request for </span>
                 <span>{item.type}</span>
               </div>
-            }
-          />
+            </div>
+          </div>
         </div>
         <div>
           <label className="text-muted font-semibold text-xs">Raised On</label>
           <div>{item.raisedOn}</div>
         </div>
-        <div className="space-x-default self-end opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+        <div className="space-x-default self-end opacity-0 group-hover/card:opacity-100 transition-opacity duration-200 text-right">
           <Button type="primary" onClick={() => setModalType("approve")}>
             Approve
           </Button>
